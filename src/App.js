44f99c6fd840df0c50206ea5d92e9c7813a9c263
Bruno Bartolomeu <bruno.bartolomeu5@hotmail.com>
2021-12-17 18:16:47 -0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ContextProvider from './context/ContextProvider';
-import './App.css';
-import CardForm from './components/CardForm';
-import Cards from './components/Cards';
-import Filters from './components/Filters';
+import React from "react";
+import ContextProvider from "./context/ContextProvider";
+import "./App.css";
+import CardForm from "./components/CardForm";
+import Cards from "./components/Cards";
+import Filters from "./components/Filters";
 function App() {
   return (
     <div className="App">
@@ -11,6 +11,17 @@ function App() {
         <CardForm />
         <Filters />
         <Cards />
+        <footer>
+          <a
+            href="https://github.com/BrunoCBart/Tryunfo"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github"></i>
+            <p>code</p>
+          </a>
+          <p>Projeto criado por Bruno Bartolomeu</p>
+        </footer>
       </ContextProvider>
     </div>
   );
